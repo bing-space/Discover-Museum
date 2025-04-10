4ab6a06f3aec3ccc68e9dfa408a25ed0b6e45074
Bing <bing334@gmail.com>
2025-04-10 19:43:17 -0400

    Create GET and POST routes for new museum, setup new museum form
    - create GET route for display new museum form
    - create POST route for get data from form and save to database, redirect to the new museum show page
    - add "new museum" button/link to index page
    - add "all museum" button/link to new page and show page

--- app.js
+++ app.js
@@ -22,17 +22,42 @@ const app = express();
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'))
 
+app.use(express.urlencoded({extended: true}))
 
 
+/*
+*   =============================
+*   ROUTE SECTION
+*   =============================
+*/ 
 app.get('/', (req, res) => {
     res.render('home');
 })
 
+// GET: Index Page
+//      Display all museum
 app.get('/museums', async (req,res) => {
     const museums = await Museum.find({});
     res.render('museums/index',{ museums })
 })
 
+// GET: New Page
+//      Display new museum form
+app.get('/museums/new', (req, res) => {
+    res.render('museums/new');
+})
+
+// POST: Get data from new museum form,
+//       save to database,
+//       redirect to the new museum show page
+app.post('/museums', async (req, res) => {
+    const theMuseum = new Museum(req.body.museum);
+    await theMuseum.save();
+    res.redirect(`/museums/${theMuseum._id}`)
+})
+
+// GET: Show Page
+//      Display museum detail based of id
 app.get('/museums/:id', async (req,res) => {
     const theMuseum = await Museum.findById(req.params.id);
     res.render('museums/show', { theMuseum })
